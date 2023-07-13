df1baee0baf113e9e2e233391019d0857a4d979a
Pedro Henrique <pedrohenrique01052006@gmail.com>
2023-07-13 19:00:34 -0300

    Inicializando repositório

--- src/main/java/frc/robot/DriverController.java
+++ src/main/java/frc/robot/DriverController.java
@@ -1,26 +1,96 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 
 public class DriverController {
 
-    public final XboxController controller = new XboxController(0);
+    public final XboxController controller;
 
-    public final JoystickButton xButton = new JoystickButton(controller, XboxController.Button.kX.value);
-    public final JoystickButton aButton = new JoystickButton(controller, XboxController.Button.kA.value);
-    public final JoystickButton bButton = new JoystickButton(controller, XboxController.Button.kB.value);
-    public final JoystickButton yButton = new JoystickButton(controller, XboxController.Button.kY.value);
-    public final JoystickButton leftBumper = new JoystickButton(controller, XboxController.Button.kLeftBumper.value);
-    public final JoystickButton rightBumper = new JoystickButton(controller, XboxController.Button.kRightBumper.value);
-    public final JoystickButton startButton = new JoystickButton(controller, 6);
-    public final JoystickButton selectButton = new JoystickButton(controller, 7);
-    public final POVButton XUp = new POVButton(controller, 0);
-    public final POVButton XDown = new POVButton(controller, 180);
-    public final POVButton XRight = new POVButton(controller, 90);
-    public final POVButton XLeft = new POVButton(controller, 270);
+    private final JoystickButton xButton;
+    private final JoystickButton aButton;
+    private final JoystickButton bButton;
+    private final JoystickButton yButton;
+    private final JoystickButton leftBumper;
+    private final JoystickButton rightBumper;
+    private final JoystickButton startButton;
+    private final JoystickButton selectButton;
+    private final POVButton xUp;
+    private final POVButton xDown;
+    private final POVButton xRight;
+    private final POVButton xLeft;
 
+    public DriverController() {
+        this.controller = new XboxController(0);
+        this.xButton = new JoystickButton(controller, XboxController.Button.kX.value);
+        this.aButton = new JoystickButton(controller, XboxController.Button.kA.value);
+        this.bButton = new JoystickButton(controller, XboxController.Button.kB.value);
+        this.yButton = new JoystickButton(controller, XboxController.Button.kY.value);
+        this.leftBumper = new JoystickButton(controller, XboxController.Button.kLeftBumper.value);
+        this.rightBumper = new JoystickButton(controller, XboxController.Button.kRightBumper.value);
+        this.startButton = new JoystickButton(controller, 6);
+        this.selectButton = new JoystickButton(controller, 7);
+        this.xUp = new POVButton(controller, 0);
+        this.xDown = new POVButton(controller, 180);
+        this.xRight = new POVButton(controller, 90);
+        this.xLeft = new POVButton(controller, 270);
+    }
+
+    // Face buttons
+    public void whileXButton(Command command) {
+        xButton.whileTrue(command);
+    }
+    public void whileAButton(Command command) {
+        aButton.whileTrue(command);
+    }
+    public void whileBButton(Command command) {
+        bButton.whileTrue(command);
+    }
+    public void whileYButton(Command command) {
+        yButton.whileTrue(command);
+    }
+
+    // D-Pad
+    public void whileXUp(Command command) {
+        xUp.whileTrue(command);
+    }
+    public void whileXDown(Command command) {
+        xDown.whileTrue(command);
+    }
+    public void whileXRight(Command command) {
+        xRight.whileTrue(command);
+    }
+    public void whileXLeft(Command command) {
+        xLeft.whileTrue(command);
+    }
+    
+    // Shoudler buttons
+    public void whileLeftBumper(Command command) {
+        leftBumper.whileTrue(command);
+    }
+    public void whileRightBumper(Command command) {
+        rightBumper.whileTrue(command);
+    }
+
+    // Triggers
+    public double getLeftTrigger() {
+        return controller.getLeftTriggerAxis();
+    }
+    public double getRightTrigger() {
+        return controller.getRightTriggerAxis();
+    }
+    
+    // Start and select
+    public void whileStartButton(Command command) {
+        startButton.whileTrue(command);
+    }
+    public void whileSelectButton(Command command) {
+        selectButton.whileTrue(command);
+    }
+
+    // Sticks
     public double getLeftX() {
         return controller.getLeftX();
     }
@@ -32,9 +102,8 @@ public class DriverController {
     public double getRightX() {
         return controller.getRightX();
     }
-    
+
     public double getRightY() {
         return controller.getRightY();
     }
-
 }
